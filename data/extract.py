da9f4892ed27c655b5c9a6a74899613e7186313e
Gatram Eshwar sai <gatram.sai.19cse@bmu.edu.in>
2022-11-08 02:12:42 +0530

    changes made to data

--- data/extract.py
+++ data/extract.py
@@ -1,14 +1,51 @@
 import pandas as pd
+import numpy as np
+import json
+annual=pd.read_csv("./csv/annual.csv")
+annual=annual.set_index('year')
 
-files=["./csv/annual.csv","./csv/cummulative.csv","./csv/university.csv"]
+cummulative=pd.read_csv('./csv/cummulative.csv')
+cummulative=cummulative.set_index('year')
+
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
 
 
 
 
-def extract(file):
-    df=pd.read_csv(file)
-    outputFile=file.split('/')[2].split('.')[0]
-    df.to_json(f"./json/{outputFile}.json")
 
-for file in files:
-    extract(file)
+def extract(df):
+    data=[]
+    cols=df.columns
+    for col in cols:
+        
+        df.loc[df[col]<10,col]=0
+        myDict=dict(df[col])
+        temp={
+            "name":col,
+            "data":myDict,
+        }
+        data.append(temp)
+
+    return data
+
+
+annualData=extract(annual)
+
+with open('./json/annual.json','w') as outFile:
+        outFile.write(json.dumps(annualData,cls=NpEncoder))
+
+cummulativeData=extract(cummulative)
+
+with open('./json/cummulative.json','w') as outFile:
+        outFile.write(json.dumps(cummulativeData,cls=NpEncoder))
